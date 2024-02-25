4c30bab349b818e61c3f2ae37e1c5788cb3f4ad8
김재윤 <jaeyun1723@naver.com>
2024-02-25 18:20:42 +0900

    feat. initial commit #[Issue Number]

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+=
+src/Main.java,9\2\924f089edb4af5ee45c8a9d1129cd28e2b368138
+>
+src/pumas.yaml,9\f\9f2395dfb6a4b44df9f498191dc0c0926aff6b05
+A
+src/panthers.yaml,f\8\f81076b6b846ae5837189232691b152982258720
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hi");
+        System.out.println("Hello world!");
+        System.out.println("gg");
+
+        System.out.println("Add Olivia to Leopards");
+    }
+}
