47a423fca614097d6259ddb2b68c9f76eec34bef
김재윤 <jaeyun1723@naver.com>
2024-02-25 18:55:32 +0900

    fsddsf

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hi");
         System.out.println("Hello world!");
+        System.out.println("fsddsf");
         System.out.println("Add Olivia to Leopards");
     }
 }
