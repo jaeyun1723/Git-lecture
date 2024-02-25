aafd6347f9efb219e9435be26b982fe0cd21cff3
김재윤 <jaeyun1723@naver.com>
2024-02-25 18:27:20 +0900

    #[1]

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hi");
         System.out.println("Hello world!");
-        System.out.println("ggsddsf");
+        System.out.println("ggsddsdfsf");
         System.out.println("Add Olivia to Leopards");
     }
 }
