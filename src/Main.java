c50ddac38e003789a69c11729f55b53f9f8d1ff6
김재윤 <jaeyun1723@naver.com>
2024-02-25 18:29:09 +0900

    feat. ãmain 추가 (#1)

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hi");
         System.out.println("Hello world!");
-        System.out.println("ggsddssfdfdsdfsf");
+        System.out.println("ggsddssfsddfsdfdsdfsf");
         System.out.println("Add Olivia to Leopards");
     }
 }
